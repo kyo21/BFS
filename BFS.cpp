1298ad5771180af391f96e5efa9ed48c62dfa358
jikgungde01 <nasi72873@gmail.com>
2024-01-16 12:24:49 +0800

    penambahan fungsi bilang halo
    tambahan fungsi ini untuk menampilkan kata halo

--- BFS.cpp
+++ BFS.cpp
@@ -11,6 +11,11 @@ string nama= "Saya";
 string reseller = "Paijo";
 string nama_pertama="Gede";
 
+int bilang_halo(){
+
+cout<<"haloooo wii"<<endl;
+}
+
 void display_nama(string nama){
 //
 cout<<"Nama anda: "<<nama<<endl;
